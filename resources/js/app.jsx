4d83bf679ccb0abe815c8b4d9b0b25b210048794
husni0204 <husnisale@gmail.com>
2022-10-13 14:20:24 +0700

    tambah tailwind css

---
+++ resources/js/app.jsx
@@ -0,0 +1,17 @@
+import "./bootstrap";
+
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+if (document.getElementById("root")) {
+    const rootElement = document.getElementById("root");
+    const root = createRoot(rootElement);
+
+    root.render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}
